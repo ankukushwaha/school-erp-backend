f22ede6332ce15c7cb2d3633de910724f2277377
Ankur Kushwaha <ankur@zentory.ai>
2026-04-06 00:08:50 +0530

    setup for showing api on swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('My APIs')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(7240, '0.0.0.0');
   console.log(`Server running on http://localhost:${7240}`);
